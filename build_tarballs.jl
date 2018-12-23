03fd1c74ac820dbff17ae49208196165954ceed9
Juan Pablo Vielma <jvielma@mit.edu>
2018-12-23 12:40:49 -0300

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -30,7 +30,7 @@ mkdir build
 cd build/
 ## STATIC BUILD START
 if [ $target = "x86_64-apple-darwin14" ]; then
-  export AR=/opt/x86_64-apple-darwin14/bin/llvm-ar
+  export AR=/opt/x86_64-apple-darwin14/bin/x86_64-apple-darwin14-ar
 fi
 ../configure --prefix=$prefix --with-pic --disable-pkg-config --host=${target} --disable-shared --enable-static --enable-dependency-linking lt_cv_deplibs_check_method=pass_all \
 --with-metis-lib="-L${prefix}/lib -lcoinmetis" --with-metis-incdir="$prefix/include/coin/ThirdParty" \
